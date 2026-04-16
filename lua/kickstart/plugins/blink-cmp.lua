e4004272cd31a875ef01cb8e2dfbe57e4b6b27ef
Grayson Gall <ggall713@gmail.com>
2026-04-15 23:28:27 -0400

    Removing the annoyingly aggressive autocomplete

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -93,7 +93,7 @@ return {
       fuzzy = { implementation = 'lua' },
 
       -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
+      signature = { enabled = false },
     },
   },
 }
